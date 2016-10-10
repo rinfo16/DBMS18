5b55711620b03a737d1adecb2cb980791caa037f
guohua <fchdir@qq.com>
2016-10-10 18:18:04 +0800

    build arguments

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
+
+CMAKE_BUILD_TYPE=Debug
+COVERAGE=0
+CMAKE_ARGS=""
+
+while [ -n "$*" ]
+do
+  case "$1" in
+    -debug)
+      CMAKE_ARGS+=" -DCMAKE_BUILD_TYPE=Debug"
+      ;;
+    -release)
+      CMAKE_ARGS+=" -DCMAKE_BUILD_TYPE=Release"
+      ;;
+    -coverage)
+      CMAKE_ARGS+=" -DENABLE_GCOV=1"
+      ;;
+    -help|*)
+      echo "help ... TODO ...."
+      exit 1
+      ;;
+    esac
+  shift;
+done
+
 cur_dir=$(pwd)
 scrip_dir=$(cd `dirname $0`; pwd)
 mkdir -p $scrip_dir/../build
-cmake -H$scrip_dir/../src -B$scrip_dir/../build
+cmake -H$scrip_dir/../src -B$scrip_dir/../build ${CMAKE_ARGS}
 
--- src/unit_test/hello.test.cpp
+++ src/unit_test/hello.test.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "common/define.h"
 int main(int argc, const char* argv[])
 {
 	std::cout<<"Hello world !!!" << std::endl;
