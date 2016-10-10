f17111ce3e29cce759f1ab9e607611bbfcd17238
guohua <fchdir@qq.com>
2016-10-10 19:54:10 +0800

    include stdint.h

--- src/include/common/define.h
+++ src/include/common/define.h
@@ -1,14 +1,14 @@
 #ifndef DEFINE_H_
 #define DEFINE_H_
 
-#include <cstdint>
-#include <cstdlib>
+#include <stdint.h>
+#include <stdlib.h>
 
-typedef std::uint32_t fileno_t;
-typedef std::uint32_t pageno_t;
-typedef std::uint32_t slotno_t;
-typedef std::uint32_t relationid_t;
-typedef std::uint32_t attributeid_t;
-typedef std::uint64_t cursor_t;
+typedef uint32_t fileno_t;
+typedef uint32_t pageno_t;
+typedef uint32_t slotno_t;
+typedef uint32_t relationid_t;
+typedef uint32_t attributeid_t;
+typedef uint64_t cursor_t;
 
 #endif // DEFINE_H_
