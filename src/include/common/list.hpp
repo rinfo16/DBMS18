ee3d04be98f96d3b56b6c4dda744c0eb781cded7
Lily1756 <Lily1756>
2016-10-20 11:49:40 +0800

    list.hpp

--- src/include/common/list.hpp
+++ src/include/common/list.hpp
@@ -14,29 +14,54 @@ class ListNode : public T {
         next_(NULL) {
   }
 
+//judge if node is the first one
   bool IsFirst() const {
-    return false;
+      return false;
   }
 
+//judge if the node is the last one
   bool IsLast() const {
     return false;
   }
+
+//insert node on the right end
   void LinkToRight(ListNode<T> *right_node) {
     next_ = right_node;
     right_node->prev_ = this;
+
   }
 
-  void LinkToLeft(ListNode<T> *left_node) {
+
+
+
+//insert node on the left end
+  void LinkToLeft(ListNode <T> *left_node) {
+    left_node->next_=this;
+    this->prev_=left_node;
   }
 
+//return the first node
   ListNode<T>* First() {
-    return NULL;
+    ListNode <T> * p;
+    p=this;
+    while (p->prev_!=NULL)
+    {
+       p=p->prev_;
+     }
+     return p;
   }
 
+//return the last node
   ListNode<T>* Last() {
-    return NULL;
+   ListNode <T> * p;
+   while (p->next_!=NULL)
+     {
+       p=p->next_;
+     }
+     return p;
   }
 
+//delete one node
   void Remove() {
     if (prev_ != NULL) {
       prev_->next_ = next_;
@@ -44,28 +69,35 @@ class ListNode : public T {
     if (next_ != NULL) {
       next_->prev = prev_;
     }
+
   }
+
+
+//return the next node
 
   ListNode<T>* Next() {
     return next_;
+
   }
-  ListNode<T>* Prev() {
-    return NULL;
+
+//return the prev node
+  ListNode <T> * Prev(){
+    return this->prev_;
   }
 
  private:
-  ListNode<T> *prev_;
-  ListNode<T> *next_;
+  ListNode <T> *prev_;
+  ListNode <T> *next_;
 };
 
-template<typename T>
-class List {
+template <typename T>
+class List:public T {
  public:
   List()
-      : head_(NULL),
+       :head_(NULL),
         tail_(NULL) {
-  }
-  ;
+    }
+
   void PushBack(ListNode<T> *node) {
     if (tail_ == NULL) {
       assert(head_ == NULL);
@@ -77,6 +109,14 @@ class List {
     }
   }
   void PushFront(ListNode<T> *node) {
+    if (head_ == NULL) {
+      assert(tail_ == NULL);
+      tail_ = node;
+      head_ = node;
+    } else {
+      head_->LinkToLeft(node);
+      head_ = node;
+    }
   }
   ListNode<T>* PopFront() {
     ListNode<T> *node = head_;
@@ -93,8 +133,20 @@ class List {
   }
 
   ListNode<T>* PopBack() {
-    return NULL;
+    ListNode <T> * node =tail_;
+    if (head_==tail_){
+      assert(tail_==NULL || tail_->Prev()==NULL);
+      assret(head_==NULL || head_->Next()==NULL);
+      tail_=NULL;
+      head_=NULL;
+    }
+    else{
+      tail_=tail_->prev_;
+    }
+
+    return node;
   }
+
 
   void Remove(ListNode<T> *node) {
     if (node == head_) {
@@ -105,12 +157,10 @@ class List {
     }
     node->Remove();
   }
-
- private:
-  ListNode<T> *head_;
-  ListNode<T> *tail_;
+private:
+  ListNode <T> * head_;
+  ListNode <T> * tail_;
 };
-
 }  // namespace utils
 
 #endif // SRC_INCLUDE_COMMON_LIST_HPP_
