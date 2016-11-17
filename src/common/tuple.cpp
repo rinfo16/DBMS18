55879b8c6debffa83c9524b2795ee9d9be779c2c
guohua <fchdir@qq.com>
2016-11-17 13:42:09 +0800

    boost::shared_ptr

---
+++ src/common/tuple.cpp
@@ -0,0 +1,10 @@
+#include "common/tuple.h"
+
+namespace memory {
+
+Tuple CreateTuple(size_t size) {
+  RawTuple *raw_tuple = reinterpret_cast<RawTuple*>(malloc(size));
+  return boost::shared_ptr<RawTuple>(raw_tuple, &free);
+}
+
+}
