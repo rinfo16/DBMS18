555b69362fc9c3b482b153ab962894b58fded1ca
guohua <guohua@GH-PC>
2016-11-20 14:39:37 +0800

    argv fix

--- src/rrportal/portal.cpp
+++ src/rrportal/portal.cpp
@@ -11,7 +11,7 @@ using namespace boost::program_options;
 bool g_quit = false;
 
 void SigHandler(int signum) {
-  BOOST_LOG_TRIVIAL(info) << "receive signal [" << signum << "].";
+  BOOST_LOG_TRIVIAL(info)<< "receive signal [" << signum << "].";
   if (signum == SIGINT) {
     g_quit = true;
   } else if (signum == SIGQUIT) {
@@ -30,9 +30,10 @@ void RegisterSignal() {
 int main(int argc, char* argv[]) {
   try {
     options_description desc { "Options" };
-    desc.add_options()("help", "help screen")("pool_size",
-                                              "buffer pool size (byte)")(
-        "db_path", "datebase path")("port", "port of the server listen on");
+    desc.add_options()("help", "help screen")
+        ("pool_size", value<uint32_t>()->default_value(BUFFER_POOL_SIZE), "buffer pool size (byte)")
+        ("db_path", value<std::string>()->default_value("/tmp"), "datebase path")
+        ("port", value<uint32_t>()->default_value(8432),"port of the server listen on");
 
     variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
@@ -44,27 +45,27 @@ int main(int argc, char* argv[]) {
       return 0;
     }
     if (vm.count("pool_size")) {
-      config::Setting::instance().buffer_pool_size_ = vm["pool_size"].as<int>();
+      config::Setting::instance().buffer_pool_size_ = vm["pool_size"].as<uint32_t>();
     }
     if (vm.count("db_path")) {
       config::Setting::instance().data_directory_ =
-          vm["path"].as<std::string>();
+          vm["db_path"].as<std::string>();
     }
     if (vm.count("port")) {
       config::Setting::instance().port_ = vm["port"].as<uint32_t>();
     }
 
-    BOOST_LOG_TRIVIAL(info) << "nutshell startup .........";
+    BOOST_LOG_TRIVIAL(info)<< "nutshell startup .........";
     RegisterSignal();
     Server server(config::Setting::instance().port_);
     server.Start();
     while (true) {
       if (g_quit) {
         server.Stop();
-        BOOST_LOG_TRIVIAL(info) << "nutshell stop .........";
+        BOOST_LOG_TRIVIAL(info)<< "nutshell stop .........";
         break;
       }
-      std::this_thread::sleep_for (std::chrono::seconds(1));
+      std::this_thread::sleep_for(std::chrono::seconds(1));
     }
   } catch (const error &ex) {
     std::cerr << ex.what() << '\n';
