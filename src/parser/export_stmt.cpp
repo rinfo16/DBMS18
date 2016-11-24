7bff25a633181f30ef9fb7ebf0b2eea1ed1599ee
guohua <fchdir@qq.com>
2016-11-24 17:18:01 +0800

    export csv

---
+++ src/parser/export_stmt.cpp
@@ -0,0 +1,27 @@
+#include "parser/export_stmt.h"
+
+namespace ast {
+
+ptree ExportStmt::ToPropertyTree() const {
+  ptree export_stmt;
+  if (!table_name_.empty()) {
+    export_stmt.put(STR_TABLE_NAME, table_name_);
+  }
+  if (!opt_column_name_list_.empty()) {
+    ptree column_name_list;
+    for (size_t i = 0; i < opt_column_name_list_.size(); i++) {
+      std::stringstream ssm;
+      ssm << i;
+      ptree column_name;
+      column_name.put(STR_COLUMN_NAME, opt_column_name_list_[i]);
+      column_name_list.push_back(std::make_pair(ssm.str(), column_name));
+    }
+    export_stmt.put_child(STR_COLUMN_NAME_LIST, column_name_list);
+  }
+  if (!file_path_.empty()) {
+    export_stmt.put(STR_FILE_PATH, file_path_);
+  }
+  return export_stmt;
+}
+
+} // namespace ast
