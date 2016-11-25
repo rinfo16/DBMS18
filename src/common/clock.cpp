92fd008e6da65176d433e674ebcf84ee8daab0cf
Ubuntu <2298328881@qq.com>
2016-11-25 17:31:35 +0800

    clock.cpp

--- src/common/clock.cpp
+++ src/common/clock.cpp
@@ -1,14 +1,34 @@
 #include "common/clock.h"
-
+#include <time.h>
+#include <iostream>
 namespace utils {
 
 Clock::Clock() {
   // TODO Auto-generated constructor stub
-
+duration = 0;
+start = 0;
+stop = 0;
 }
 
 Clock::~Clock() {
   // TODO Auto-generated destructor stub
 }
 
+void Clock::Start(){
+  start = clock();
+}
+
+void Clock::Stop(){
+  stop = clock();
+  duration = stop-start;
+}
+uint64_t Clock::Elapse() const {
+  return duration;
+}
+std::string Clock::Format() const {
+  std::cout << "elapsed time = ";
+  std::cout << duration;
+  std::cout << "ms" << std::endl;
+//  printf("elapsed time = %ld ms",duration);
+}
 }  // namespace utils
